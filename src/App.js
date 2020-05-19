c82d835b7d148cd4cb3c951f93ccac7fe35b945e
Vasu Rabaib <vrabaib@e1z2r3p33.42.us.org>
2020-05-19 11:27:19 -0700

    Writing Separate Functional Components for Grid Handling

--- src/App.js
+++ src/App.js
@@ -7,41 +7,42 @@ function App() {
       <h1>Red Tetris</h1>
       <div className="game-container">
         <div className="tetris-grid">
-          <Grid row="20" column="10"/>
+          <PlayerGrid row="20" column="10"/>
         </div>
         <div className="tetris-grid">
-          <Grid row="20" column="10"/>
+          <PlayerGrid row="20" column="10"/>
         </div>
       </div>
     </div>
   );
 }
 
-const initTetrisGrid = ({row, column, color}) => {
-  let grid = [];
-  for (let i = 0; i < +row; i++) {
-    const singleRow = Array(+column).fill(color);
-    grid.push(singleRow);
-  }
-  return grid;
+
+
+const PlayerGrid = ({ row, column }) => {
+  const color = "black";
+  const [ grid, setGrid ] = useState(initTetrisGrid( row, column, color ));
+  return renderGrid(grid);
 }
 
-const Grid = ({row, column}) => {
-  const color = "black";
-  const [ grid, setGrid ] = useState(initTetrisGrid({ row, column, color }));
-  console.log(setGrid);
-  return (
-    grid.map((row, rIdx) => {
-      return (
-        <div className="tetris-row" key={`r${rIdx}`}>
-          {row.map((cellColor, cIdx) => {
-            return <Cell key={`r${rIdx}c${cIdx}`} color={cellColor}/>
-          })}
-        </div>
-      );
-    })
-  );
+const initTetrisGrid = ( row, column, color ) => {
+  const singleRow = Array(+column).fill(color);
+  return Array(+row).fill(singleRow);
 }
+
+const renderGrid = grid => {
+  return (
+    grid.map((row, rIdx) =>
+      <div className="tetris-row" key={`r${rIdx}`}>
+        {row.map((cellColor, cIdx) =>
+          <Cell key={`r${rIdx}c${cIdx}`} color={cellColor}/>
+        )}
+      </div>
+    )
+  )
+}
+
+const renderRows = render
 
 // const Row = ({cellAmount}) => {
 //     let row = [];
